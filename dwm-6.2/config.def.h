2c217cdaaf868edc9d325d0f483e4c9d03947ce6
Sureshkumar <suresh241098@gmail.com>
2020-05-24 14:49:29 +0530

    all working

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -15,20 +15,21 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 
 static const char col_pink[]		= "#ff54e3";
+static const char col_neon[]		= "#00fa21";
 static const char col_purple[]		= "#69005e";
 static const char col_cyan[]        = "#005577";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_pink  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_neon  },
 	
 	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	
-	[SchemeTagsSel]  = { col_gray4, col_purple,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_neon,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
     
-    [SchemeInfoSel]  = { col_gray4, col_purple,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { col_gray4, col_neon,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
     [SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
